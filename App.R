177544f389be86da64181959ca0bf121b47a7bf6
gilles-guillot <gilles.b.guillot@gmail.com>
2025-08-07 18:07:14 +0200

    library() in ui and server .r -> .R for ui and server date-modified -> last-modfied

--- App.R
+++ App.R
@@ -8,8 +8,12 @@ library(DT)
 if (!dir.exists("Norms&Standards")) dir.create("Norms&Standards")
 if (!dir.exists("Output")) dir.create("Output")
 
-source("ui.R")
-source("server.R")
+source("ui.r")
+source("server.r")
 
 # --- Run App ---
 shinyApp(ui = ui, server = server)
+
+
+
+
